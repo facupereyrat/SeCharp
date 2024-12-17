b14cf73feefae07bb1e28dca9d5a567821db251f
facupereyrat <faucpereyrat@outlook.com>
2024-12-16 21:44:30 -0300

    Avances y configuraciones

--- OperadoresAritmeticos/FrmOpAritmeticos.Designer.cs
+++ OperadoresAritmeticos/FrmOpAritmeticos.Designer.cs
@@ -29,6 +29,15 @@
         private void InitializeComponent()
         {
             this.btnSuma = new System.Windows.Forms.Button();
+            this.lblnumero1 = new System.Windows.Forms.Label();
+            this.lblnumero2 = new System.Windows.Forms.Label();
+            this.txtnumero1 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtresultado = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lblresultado = new System.Windows.Forms.Label();
+            this.txtnumero2 = new System.Windows.Forms.TextBox();
+            this.panel3 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
             // btnSuma
@@ -36,12 +45,100 @@
             this.btnSuma.BackColor = System.Drawing.Color.Yellow;
             this.btnSuma.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnSuma.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSuma.Location = new System.Drawing.Point(126, 260);
+            this.btnSuma.Location = new System.Drawing.Point(166, 199);
             this.btnSuma.Name = "btnSuma";
             this.btnSuma.Size = new System.Drawing.Size(56, 38);
             this.btnSuma.TabIndex = 0;
             this.btnSuma.Text = "+";
             this.btnSuma.UseVisualStyleBackColor = false;
+            this.btnSuma.Click += new System.EventHandler(this.btnSuma_Click);
+            // 
+            // lblnumero1
+            // 
+            this.lblnumero1.AutoSize = true;
+            this.lblnumero1.Font = new System.Drawing.Font("Consolas", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblnumero1.ForeColor = System.Drawing.Color.White;
+            this.lblnumero1.Location = new System.Drawing.Point(146, 56);
+            this.lblnumero1.Name = "lblnumero1";
+            this.lblnumero1.Size = new System.Drawing.Size(100, 22);
+            this.lblnumero1.TabIndex = 1;
+            this.lblnumero1.Text = "Numero 1:";
+            // 
+            // lblnumero2
+            // 
+            this.lblnumero2.AutoSize = true;
+            this.lblnumero2.Font = new System.Drawing.Font("Consolas", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblnumero2.ForeColor = System.Drawing.Color.White;
+            this.lblnumero2.Location = new System.Drawing.Point(146, 90);
+            this.lblnumero2.Name = "lblnumero2";
+            this.lblnumero2.Size = new System.Drawing.Size(100, 22);
+            this.lblnumero2.TabIndex = 2;
+            this.lblnumero2.Text = "Numero 2:";
+            // 
+            // txtnumero1
+            // 
+            this.txtnumero1.BackColor = System.Drawing.SystemColors.InfoText;
+            this.txtnumero1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtnumero1.ForeColor = System.Drawing.SystemColors.InactiveBorder;
+            this.txtnumero1.Location = new System.Drawing.Point(252, 59);
+            this.txtnumero1.Name = "txtnumero1";
+            this.txtnumero1.Size = new System.Drawing.Size(200, 13);
+            this.txtnumero1.TabIndex = 3;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(252, 73);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 2);
+            this.panel1.TabIndex = 4;
+            // 
+            // txtresultado
+            // 
+            this.txtresultado.BackColor = System.Drawing.SystemColors.InfoText;
+            this.txtresultado.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtresultado.ForeColor = System.Drawing.SystemColors.InactiveBorder;
+            this.txtresultado.Location = new System.Drawing.Point(252, 295);
+            this.txtresultado.Name = "txtresultado";
+            this.txtresultado.Size = new System.Drawing.Size(200, 13);
+            this.txtresultado.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Location = new System.Drawing.Point(252, 110);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 2);
+            this.panel2.TabIndex = 4;
+            // 
+            // lblresultado
+            // 
+            this.lblresultado.AutoSize = true;
+            this.lblresultado.Font = new System.Drawing.Font("Consolas", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblresultado.ForeColor = System.Drawing.Color.White;
+            this.lblresultado.Location = new System.Drawing.Point(146, 290);
+            this.lblresultado.Name = "lblresultado";
+            this.lblresultado.Size = new System.Drawing.Size(110, 22);
+            this.lblresultado.TabIndex = 2;
+            this.lblresultado.Text = "Resultado:";
+            // 
+            // txtnumero2
+            // 
+            this.txtnumero2.BackColor = System.Drawing.SystemColors.InfoText;
+            this.txtnumero2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtnumero2.ForeColor = System.Drawing.SystemColors.InactiveBorder;
+            this.txtnumero2.Location = new System.Drawing.Point(252, 96);
+            this.txtnumero2.Name = "txtnumero2";
+            this.txtnumero2.Size = new System.Drawing.Size(200, 13);
+            this.txtnumero2.TabIndex = 3;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.White;
+            this.panel3.Location = new System.Drawing.Point(252, 309);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(200, 2);
+            this.panel3.TabIndex = 4;
             // 
             // FrmOpAritmeticos
             // 
@@ -49,15 +146,36 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(10)))), ((int)(((byte)(20)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtnumero2);
+            this.Controls.Add(this.txtresultado);
+            this.Controls.Add(this.txtnumero1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lblresultado);
+            this.Controls.Add(this.lblnumero2);
+            this.Controls.Add(this.lblnumero1);
             this.Controls.Add(this.btnSuma);
+            this.Font = new System.Drawing.Font("Consolas", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "FrmOpAritmeticos";
             this.Text = "FrmOpAritmeticos";
+            this.Load += new System.EventHandler(this.FrmOpAritmeticos_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btnSuma;
+        private System.Windows.Forms.Label lblnumero1;
+        private System.Windows.Forms.Label lblnumero2;
+        private System.Windows.Forms.TextBox txtnumero1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtresultado;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lblresultado;
+        private System.Windows.Forms.TextBox txtnumero2;
+        private System.Windows.Forms.Panel panel3;
     }
 }
--- OperadoresAritmeticos/FrmOpAritmeticos.cs
+++ OperadoresAritmeticos/FrmOpAritmeticos.cs
@@ -15,5 +15,20 @@ namespace CursoCsharp.OperadoresAritmeticos
         {
             InitializeComponent();
         }
+        int numero1;
+        int numero2;
+        int resultado;
+        private void FrmOpAritmeticos_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSuma_Click(object sender, EventArgs e)
+        {
+            numero1 = Convert.ToInt32(txtnumero1.Text);
+            numero2 = Convert.ToInt32(txtnumero2.Text);
+
+
+        }
     }
 }
